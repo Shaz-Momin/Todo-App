385318d6139154d263ddbb5e4a42e72bda2304d5
Shaz Momin <shazmomin@MacBook-Pro.local>
2020-07-28 10:53:51 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: ''}
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      key: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item => item.key !== key);
+    this.setState({
+      items: filteredItems
+    })
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <h2>TODO List</h2>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <input id="new-todo" placeholder="Add new tasks" value={this.state.text} onChange={this.handleChange}/>
+          <button type="submit" onClick={this.handleSubmit}>ADD</button>
+        </form>
+        <div className="content">
+          <ul>
+            {this.state.items.map(item => (
+              <li key={item.key}><span onClick={() => this.deleteItem(item.key)}>&#10006;</span> {item.text}</li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
+
+  
+
 export default App;
+
